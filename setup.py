f2ffabbcf8ac21e7106629e274784aa6092c97aa
Wait for it <alexandre.martin@satalia.com>
2017-10-13 15:34:25 +0100

    Put requirements in setup.py, because pip couldnt read req.txt

--- setup.py
+++ setup.py
@@ -17,6 +17,22 @@ setup(
     packages=['solveengine', 'solveengine/examples'],
     license='',
     url='https://github.com/Satalia/solveengine-python-modeling-lib',
-    download_url='https://github.com/Satalia/solveengine-python-modeling-lib/archive/0.3.tar.gz'
+    download_url='https://github.com/Satalia/solveengine-python-modeling-lib/archive/0.3.tar.gz',
+    install_requires=[beautifulsoup4==4.6.0,
+                      certifi==2017.7.27.1,
+                      chardet==3.0.4,
+                      enum==0.4.6,
+                      google==1.9.3,
+                      google.api==0.1.11,
+                      grpc==0.3.post19,
+                      grpcio==1.3.5,
+                      grpcio-tools==1.3.5,
+                      idna==2.6,
+                      msgpack-python==0.4.8,
+                      protobuf==3.3.0,
+                      requests==2.18.4,
+                      six==1.10.0,
+                      six==1.10.0,
+                      urllib3==1.22]
 )
 
